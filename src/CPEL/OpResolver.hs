1013885d7a00ad58bd86dce7e4728633d74ed5fa
CoderPuppy <coderpup@gmail.com>
2022-08-07 11:58:18 -0400

    Fix some (potential) bugs in the operator resolver

--- src/CPEL/OpResolver.hs
+++ src/CPEL/OpResolver.hs
@@ -15,201 +15,214 @@ import CPEL.Types
 import CPEL.CST qualified as CST
 import CPEL.Trie qualified as Trie
 
-data Syn s c
-	= SynOp
+-- | Expressions, generalized over base expressions
+data Expr e
+	-- | Embedded base expression
+	= EBase e
+	-- | Application expression
+	| EAp (Spanned (Expr e)) (Spanned (Expr e))
+	-- | Operator expression (including plain names)
+	| EOp
+		-- | Leading name part, may be empty in postfixy operators
 		(Spanned NamePart)
-		[(Spanned (Chk s c), Spanned NamePart)]
+		-- | Sub-expressions and following name parts
+		--
+		-- The last name part may be empty in prefixy operators
+		(List0 (Spanned (Expr e), Spanned NamePart))
+		-- | Variable-scoped De Bruijn index
 		(Maybe (Spanned Idx))
-	| SynExp s
-	| SynAp (Spanned (Syn s c)) (Spanned (Chk s c))
-	deriving (Show)
-instance Bifunctor Syn where
-	bimap ms mc (SynOp head parts index) = SynOp head (fmap (first $ fmap $ bimap ms mc) parts) index
-	bimap ms mc (SynExp s) = SynExp $ ms s
-	bimap ms mc (SynAp f a) = SynAp (fmap (bimap ms mc) f) (fmap (bimap ms mc) a)
-data Chk s c
-	= ChkExp c
-	| ChkSyn (Syn s c)
-	deriving (Show)
-instance Bifunctor Chk where
-	bimap ms mc (ChkExp c) = ChkExp $ mc c
-	bimap ms mc (ChkSyn s) = ChkSyn $ bimap ms mc s
+	deriving (Show, Functor)
 
-type Env = Trie.Trie NamePiece [S.Set Name]
-data OpFrame s c = OpFrame
+-- | Environment of available operators, mapped to precedence information
+type Env = Trie.Trie NamePiece (List0 (S.Set Name))
+
+-- | Parser stack
+type Stack e = List0 (Frame e)
+-- | Parser frame
+type Frame e = Either (OpFrame e) (ExpFrame e)
+-- | Operator frame, represents an operator being parsed
+data OpFrame e = OpFrame
+	-- | Leading name part
 	{ _ofrHead :: Spanned NamePart
-	, _ofrParts :: List0 (Spanned (Chk s c), Spanned NamePart)
-	, _ofrRest :: Either NamePart (List0 (Spanned (Chk s c)))
+	-- | Filled holes
+	, _ofrParts :: List0 (Spanned (Expr e), Spanned NamePart)
+	-- | Extra name pieces
+	, _ofrRest :: NamePart
+	-- | Variable-scoped De Bruijn index
 	, _ofrIndex :: Maybe (Spanned Idx)
 	}
-data ExpFrame s c = ExpFrame
-	{ _efrHead :: Spanned (Syn s c)
-	, _efrArgs :: List0 (Spanned (Chk s c))
-	}
-type Frame s c = Either (OpFrame s c) (ExpFrame s c)
-type Stack s c = List0 (Frame s c)
-data Slice s c = Slice
+-- | Expression frame, represents an embedded expression
+data ExpFrame e = ExpFrame (Spanned (Expr e))
+-- | Slice through the parser stack
+data Slice e = Slice
+	-- | The operator at the top-level of `_slExp`
 	{ _slExpName :: Maybe Name
-	, _slExp :: Spanned (Syn s c)
-	, _slStack :: Stack s c
+	-- | Inner expression
+	, _slExp :: Spanned (Expr e)
+	-- | Remaining stack
+	, _slStack :: Stack e
 	}
 
-ofrName :: OpFrame s c -> Name
+-- | Extract the operator name from an `OpFrame`
+ofrName :: OpFrame e -> Name
 ofrName OpFrame{..} = concat
 	[ snd _ofrHead
 	, reverse _ofrParts >>= snd . snd
-	, either id (const []) _ofrRest ]
+	, _ofrRest ]
 
-ofrSliceName :: OpFrame s c -> Maybe Name
-ofrSliceName OpFrame{ _ofrParts = [], _ofrRest = rest }
-	| either null (const True) rest = Nothing
-ofrSliceName ofr = Just $ ofrName ofr
-
-ofrPlace :: OpFrame s c -> Int
+-- | Compute the index of the current hole in an `OpFrame`
+ofrPlace :: OpFrame e -> Int
 ofrPlace OpFrame{..} = length _ofrHead + sum (fmap (length . snd) _ofrParts)
 
+-- | Check if an operator is allowed in a hole in another
 allows :: Env -> Name -> Int -> Name -> Bool
 allows env outer i inner =
 	S.member inner $ (!! i) $ fromJust $ Trie.lookup outer env
 
-buildAps ::
-	Spanned (Syn s c) ->
-	List0 (Spanned (Chk s c)) ->
-	Spanned (Syn s c)
-buildAps = foldr \a@(Span _ e, _) f@(Span s _, _) ->
-	(Span s e, SynAp f a)
+-- | Build an operator expression with a span
+buildOp ::
+	Spanned NamePart ->
+	List0 (Spanned (Expr e), Spanned NamePart) ->
+	Maybe (Spanned Idx) ->
+	Spanned (Expr e)
+buildOp head parts index =
+	(
+		Span (spanStart $ fst head) $
+			maybe id (max . spanEnd . fst) index $
+			case parts of
+				[] -> spanEnd $ fst head
+				parts -> spanEnd $ fst $ snd $ last parts,
+		EOp head parts index
+	)
 
-buildOpFr :: OpFrame s c -> Spanned (Syn s c)
-buildOpFr OpFrame{..} = buildAps core $ fromRight' _ofrRest
-	where
-		core = (
-				Span (spanStart $ fst _ofrHead) $
-					maybe id (max . spanEnd . fst) _ofrIndex $
-					case _ofrParts of
-						[] -> spanEnd $ fst _ofrHead
-						parts | (_, (Span _ e, _)) <- last parts -> e,
-				SynOp _ofrHead _ofrParts _ofrIndex
-			)
-
-buildFrame :: Frame s c -> Spanned (Syn s c)
-buildFrame (Left ofr) = buildOpFr ofr
-buildFrame (Right (ExpFrame head args)) = buildAps head args
-
-startSlice :: Stack s c -> Maybe (Slice s c)
-startSlice (Left ofr@OpFrame{ _ofrRest = Right args, .. }:stack)
-	= Just Slice
-		{ _slExpName = ofrSliceName ofr
-		, _slExp = buildOpFr ofr
-		, _slStack = stack }
-startSlice (Right (ExpFrame f args):stack)
+-- | Start a slice
+startSlice :: Stack e -> Maybe (Slice e)
+startSlice (Right (ExpFrame e):stack)
 	= Just Slice
 		{ _slExpName = Nothing
-		, _slExp = buildAps f args
+		, _slExp = e
 		, _slStack = stack }
 startSlice _ = Nothing
 
-extendSlice :: Env -> Slice s c -> Maybe (Slice s c)
+-- | Extend a slice by moving a frame into the expression
+extendSlice :: Env -> Slice e -> Maybe (Slice e)
+-- Prevent disallowed operators in the last hole
 extendSlice env Slice
 	{ _slExpName = Just nameE
-	, _slStack = Left ofr@OpFrame { _ofrRest = Left [""] }:_ }
+	, _slStack = Left ofr@OpFrame { _ofrRest = [""] }:_ }
 	| not $ allows env nameOp (length nameOp - 2) nameE
 	= Nothing
 	where nameOp = ofrName ofr
+-- Fill the last hole of a prefixy operator
 extendSlice env Slice
 	{ _slExp = e@(Span _ pos, _)
-	, _slStack = Left ofr@OpFrame{ _ofrRest = Left [""] }:stack }
+	, _slStack = Left ofr@OpFrame{ _ofrRest = [""] }:stack }
 	= Just Slice
 		{ _slExpName = Just $ ofrName ofr
-		, _slExp = buildOpFr $ ofr
-				{ _ofrParts =
-						(second ChkSyn e, (Span pos pos, [""])):_ofrParts ofr
-				, _ofrRest = Right [] }
+		, _slExp = buildOp
+			(_ofrHead ofr)
+			((e, (Span pos pos, [""])):_ofrParts ofr)
+			(_ofrIndex ofr)
 		, _slStack = stack }
+-- Build an application
 extendSlice env Slice
-	{ _slExp = e
-	, _slStack = Left ofr@OpFrame{ _ofrRest = Right args }:stack }
-	= Just Slice
-		{ _slExpName = ofrSliceName ofr
-		, _slExp = buildOpFr $ ofr
-				{ _ofrRest = Right $ second ChkSyn e:args }
-		, _slStack = stack }
-extendSlice env Slice
-	{ _slExp = e
-	, _slStack = Right (ExpFrame f args):stack }
+	{ _slExp = a
+	, _slStack = Right (ExpFrame f):stack }
 	= Just Slice
 		{ _slExpName = Nothing
-		, _slExp = buildAps f args
+		, _slExp = (
+			Span (spanStart $ fst f) (spanEnd $ fst a),
+			EAp f a
+		)
 		, _slStack = stack }
 extendSlice _ _ = Nothing
 
-slices :: Env -> Stack s c -> [Slice s c]
+-- | Enumerate all valid slices
+slices :: Env -> Stack e -> [Slice e]
 slices env = maybeToList . startSlice >=> go
 	where go slice = slice:maybe [] go (extendSlice env slice)
 
-lastSlice :: Env -> Stack s c -> Maybe (Slice s c)
+-- | Compute the last valid slice
+lastSlice :: Env -> Stack e -> Maybe (Slice e)
 lastSlice env = fmap go . startSlice
 	where go slice = maybe slice go $ extendSlice env slice
 
-processOperand :: Env -> Stack s c -> Spanned (Either CST.Var (s, c)) -> Either String [Stack s c]
+-- | Push an expression frame to the stack
+--
+-- Merges into an existing `ExpFrame` to build an application
+pushExpFr :: ExpFrame e -> Stack e -> Stack e
+pushExpFr (ExpFrame e) (Right (ExpFrame f):st) =
+	Right (ExpFrame (Span (spanStart $ fst f) (spanEnd $ fst e), EAp f e)):st
+pushExpFr efr st = Right efr:st
+
+-- | Push an operation frame to the stack
+--
+-- Collapses a completed frame (`_ofrRest = []`) into an `ExpFrame`
+pushOpFr :: OpFrame e -> Stack e -> Stack e
+pushOpFr OpFrame{ _ofrRest = [], .. } =
+	pushExpFr $ ExpFrame $ buildOp _ofrHead _ofrParts _ofrIndex
+pushOpFr ofr = (Left ofr:)
+
+-- | Process a single operand
+processOperand ::
+	Env -> Stack e -> Spanned (Either CST.Var e) -> Either String [Stack e]
 processOperand env stack op = fmap join $ sequenceA $ fmap ($ (stack, op)) rules
 	where rules =
-		[ \case --start op
+		[ \case -- start op
 			(stack, (_, Left (CST.Var part idx))) -> pure do
 				(rest, _) <- Trie.toList $ Trie.descend (snd part) env
-				pure $ (:stack) $ Left OpFrame
+				pure $ flip pushOpFr stack $ OpFrame
 					{ _ofrHead = part
 					, _ofrParts = []
-					, _ofrRest = case rest of
-							[] -> Right []
-							rest -> Left rest
+					, _ofrRest = rest
 					, _ofrIndex = idx }
 			_ -> pure empty
 
 		, \case -- continue op
-			(lastSlice env -> Just sl@Slice
-					{ _slStack = Left ofr@OpFrame{ _ofrRest = Left rest }:stack }
-				, (_, Left (CST.Var (span, part) idx)))
-				| Just rest' <- stripPrefix part rest
+			(stack, (_, Left (CST.Var (span, part) idx)))
+				-- find the innermost incomplete operator
+				| Just sl@Slice{ _slStack = Left ofr@OpFrame{ _ofrRest = rest }:stack' }
+					<- lastSlice env stack
+				, Just rest' <- stripPrefix part rest
+				-- ensure the inner expression is allowed inside this operator
 				, maybe True (allows env (ofrName ofr) (ofrPlace ofr)) $ _slExpName sl
 				-> do
+					-- check there is at most one variable index per operator
 					idx' <- case (idx, _ofrIndex ofr) of
 						(Just _, Just _) -> Left "kjfdsa"
 						(a, b) -> pure $ a <|> b
-					pure $ pure $ (:stack) $ Left $ ofr
+					pure $ pure $ flip pushOpFr stack' $ ofr
 						{ _ofrParts = (:_ofrParts ofr) $
-								(second ChkSyn (_slExp sl), (span, part))
-						, _ofrRest = case drop (length part) rest of
-								[] -> Right []
-								rest' -> Left rest'
+								(_slExp sl, (span, part))
+						, _ofrRest = drop (length part) rest
 						, _ofrIndex = idx' }
 			_ -> pure empty
 
 		, \case -- start postfixy op
 			(stack, (_, Left (CST.Var part idx))) -> pure do
+				-- at any point in the stack
 				sl <- slices env stack
+				-- for any postfixy operator
 				(rest, allowed:_) <- Trie.toList $ Trie.descend ("":snd part) env
+				-- which is allowed at that point
 				guard $ maybe True (flip S.member allowed) $ _slExpName sl
+
 				let pos = spanStart $ fst $ _slExp sl
-				pure $ (:stack) $ Left OpFrame
+				pure $ flip pushOpFr (_slStack sl) $ OpFrame
 					{ _ofrHead = (Span pos pos, [""])
-					, _ofrParts = [(second ChkSyn (_slExp sl), part)]
-					, _ofrRest = case rest of
-							[] -> Right []
-							rest -> Left rest
+					, _ofrParts = [(_slExp sl, part)]
+					, _ofrRest = rest
 					, _ofrIndex = idx }
 			_ -> pure empty
 
 		, \case -- embed expression
-			(stack@(Left OpFrame{ _ofrRest = Left _ }:_), (span, Right (syne, _))) ->
-				pure $ pure $ Right (ExpFrame (span, SynExp syne) []):stack
-			(Left ofr@OpFrame{ _ofrRest = Right args }:stack, (span, Right (_, chke))) ->
-				pure $ pure $ (:stack) $ Left $ ofr
-					{ _ofrRest = Right $ (span, ChkExp chke):args }
-			(Right (ExpFrame f args):stack, (span, Right (_, chke))) ->
-				pure $ pure $ Right (ExpFrame f ((span, ChkExp chke):args)):stack
+			(stack, (span, Right e)) ->
+				pure $ pure $ pushExpFr (ExpFrame (span, EBase e)) stack
+			_ -> pure empty
 		]
 
-buildOps :: Env -> [Spanned (Either CST.Var (s, c))] -> Either String [Spanned (Syn s c)]
+-- | Process an operator (`CST.EOp`)
+buildOps :: Env -> [Spanned (Either CST.Var e)] -> Either String [Spanned (Expr e)]
 buildOps env ops = do
 	stacks <- foldl
 		(\stacks op ->
--- src/CPEL/Types.hs
+++ src/CPEL/Types.hs
@@ -3,21 +3,35 @@ module CPEL.Types (module CPEL.Types, Span(..), Pos(..)) where
 import Data.Text qualified as T
 import FlatParse.Stateful (Span(..), Pos(..))
 
+-- | List with length ≥ 0
 type List0 a = [a]
+-- | List with length ≥ 1
 type List1 a = [a]
+-- | List with length ≥ 2
 type List2 a = [a]
 
+-- | Type with an attached span
 type Spanned a = (Span, a)
 
+-- | Piece of a name without any underscores
+--
+-- May be empty, for example the `NamePart` @_!@ would consist of an empty piece and
+-- a piece containing @!@.
 type NamePiece = T.Text
+-- | Partial name - Underscore separated `NamePiece`s
+--
+-- These separate the sub-expressions in operator expressions
 type NamePart = List1 NamePiece
+-- | Full name
 type Name = NamePart
+-- | De Bruijn Index
 type Idx = Int
+-- | De Bruijn level
 type Lvl = Int
+-- | Metavariable index
 type MetaVar = Int
 
 spanStart :: Span -> Pos
 spanStart (Span s _) = s
-
 spanEnd :: Span -> Pos
 spanEnd (Span _ e) = e
